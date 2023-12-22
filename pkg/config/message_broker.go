7bc2ac4c7762cee7d48971b716b026024a723464
morvencao <lcao@redhat.com>
2023-12-22 02:30:25 +0000

    support multiple maestro instances.
    Signed-off-by: morvencao <lcao@redhat.com>

--- pkg/config/message_broker.go
+++ pkg/config/message_broker.go
@@ -11,6 +11,7 @@ import (
 type MessageBrokerConfig struct {
 	EnableMock        bool              `json:"enable_message_broker_mock"`
 	SourceID          string            `json:"source_id"`
+	ClientID          string            `json:"client_id"`
 	MessageBrokerType string            `json:"message_broker_type"`
 	MQTTOptions       *mqtt.MQTTOptions `json:"mqtt_options"`
 	MQTTConfig        string            `json:"mqtt-config-file"`
@@ -20,6 +21,7 @@ func NewMessageBrokerConfig() *MessageBrokerConfig {
 	return &MessageBrokerConfig{
 		EnableMock:        false,
 		SourceID:          "maestro",
+		ClientID:          "maestro",
 		MessageBrokerType: "mqtt",
 		MQTTConfig:        filepath.Join(GetProjectRootDir(), "secrets/mqtt.config"),
 	}
@@ -28,6 +30,7 @@ func NewMessageBrokerConfig() *MessageBrokerConfig {
 func (c *MessageBrokerConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&c.EnableMock, "enable-message-broker-mock", c.EnableMock, "Enable message broker mock")
 	fs.StringVar(&c.SourceID, "source-id", c.SourceID, "Source ID")
+	fs.StringVar(&c.ClientID, "client-id", c.ClientID, "Client ID")
 	fs.StringVar(&c.MessageBrokerType, "message-broker-type", c.MessageBrokerType, "Message broker type (default: mqtt)")
 	fs.StringVar(&c.MQTTConfig, "mqtt-config-file", c.MQTTConfig, "The config file path of mqtt broker")
 }
