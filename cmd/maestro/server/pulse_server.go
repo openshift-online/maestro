8d2e0a1c523be0ee5a4f58db288804b0971bd28e
morvencao <lcao@redhat.com>
2024-01-18 02:10:01 +0000

    defer unlock.
    Signed-off-by: morvencao <lcao@redhat.com>

--- cmd/maestro/server/pulse_server.go
+++ cmd/maestro/server/pulse_server.go
@@ -67,53 +67,54 @@ func (s *PulseServer) Start(ctx context.Context) {
 			if err != nil {
 				// ignore the error and continue to tolerate the intermittent db connection issue
 				logger.Error(fmt.Sprintf("Unable to update heartbeat for maestro instance: %s", err.Error()))
-				continue
 			}
 		case <-checkTicker.C:
 			logger.Infof("Checking maestro instances liveness and trigger statusresync for dead instances")
-			// lock the Instance with a fail-fast advisory lock context.
-			// this allows concurrent processing of many instances by one or more maestro instances exclusively.
-			lockOwnerID, acquired, err := s.lockFactory.NewNonBlockingLock(ctx, "maestro-instances-pulse-check", db.Instances)
-			if err != nil {
-				logger.Error(fmt.Sprintf("Unable to obtain the event lock: %s", err.Error()))
-				continue
-			}
-			// skip if the lock is not acquired
-			if !acquired {
-				logger.Infof("Instance %s is processed by another maestro instance, skip...", instance.ID)
-				continue
-			}
-			// Instances not pulsing within the last three check intervals are considered as dead.
-			instances, err := s.instanceDao.FindByUpdatedTime(ctx, time.Now().Add(time.Duration(int64(-3*time.Second)*s.checkInterval)))
-			if err != nil {
+			if err := s.CheckInstances(ctx); err != nil {
 				// ignore the error and continue to tolerate the intermittent db connection issue
-				logger.Error(fmt.Sprintf("Unable to get outdated maestro instances: %s", err.Error()))
-				s.lockFactory.Unlock(ctx, lockOwnerID)
-				continue
+				logger.Error(fmt.Sprintf("Unable to check maestro instances: %s", err.Error()))
 			}
-			deletedInstanceIDs := []string{}
-			for _, i := range instances {
-				deletedInstanceIDs = append(deletedInstanceIDs, i.ID)
-			}
-
-			if len(deletedInstanceIDs) > 0 {
-				// trigger statusresync for dead instances only once even if there are multiple dead instances
-				// will retry in next check if the statusresync fails
-				if err := s.sourceClient.Resync(ctx); err != nil {
-					logger.Error(fmt.Sprintf("Unable to trigger statusresync for maestro instance(s): %s", err.Error()))
-					s.lockFactory.Unlock(ctx, lockOwnerID)
-					continue
-				}
-
-				// delete dead instances, ignore the error as it will be retried in the next pulse check
-				if err := s.instanceDao.DeleteByIDs(ctx, deletedInstanceIDs); err != nil {
-					logger.Error(fmt.Sprintf("Unable to delete dead maestro instances: %s", err.Error()))
-					s.lockFactory.Unlock(ctx, lockOwnerID)
-					continue
-				}
-			}
-
-			s.lockFactory.Unlock(ctx, lockOwnerID)
 		}
 	}
+}
+
+func (s *PulseServer) CheckInstances(ctx context.Context) error {
+	logger := logger.NewOCMLogger(ctx)
+	logger.Infof("Checking maestro instances liveness and trigger statusresync for dead instances")
+	// lock the Instance with a fail-fast advisory lock context.
+	// this allows concurrent processing of many instances by one or more maestro instances exclusively.
+	lockOwnerID, acquired, err := s.lockFactory.NewNonBlockingLock(ctx, "maestro-instances-pulse-check", db.Instances)
+	// Ensure that the transaction related to this lock always end.
+	defer s.lockFactory.Unlock(ctx, lockOwnerID)
+	if err != nil {
+		return fmt.Errorf("error obtaining the event lock: %v", err)
+	}
+	// skip if the lock is not acquired
+	if !acquired {
+		return fmt.Errorf("failed to acquire the lock as another maestro instance is checking instances")
+	}
+	// Instances not pulsing within the last three check intervals are considered as dead.
+	instances, err := s.instanceDao.FindByUpdatedTime(ctx, time.Now().Add(time.Duration(int64(-3*time.Second)*s.checkInterval)))
+	if err != nil {
+		return fmt.Errorf("unable to get outdated maestro instances: %s", err.Error())
+	}
+	deletedInstanceIDs := []string{}
+	for _, i := range instances {
+		deletedInstanceIDs = append(deletedInstanceIDs, i.ID)
+	}
+
+	if len(deletedInstanceIDs) > 0 {
+		// trigger statusresync for dead instances only once even if there are multiple dead instances
+		// will retry in next check if the statusresync fails
+		if err := s.sourceClient.Resync(ctx); err != nil {
+			return fmt.Errorf("unable to trigger statusresync for maestro instance(s): %s", err.Error())
+		}
+
+		// delete dead instances, ignore the error as it will be retried in the next pulse check
+		if err := s.instanceDao.DeleteByIDs(ctx, deletedInstanceIDs); err != nil {
+			return fmt.Errorf("unable to delete dead maestro instances: %s", err.Error())
+		}
+	}
+
+	return nil
 }
