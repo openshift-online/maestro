b66cad7c7c30c63452a87876c4ec234d32a0a6be
morvencao <lcao@redhat.com>
2024-01-31 14:56:24 +0000

    add more explanation for subscription type.
    Signed-off-by: morvencao <lcao@redhat.com>

--- pkg/config/pulse_server.go
+++ pkg/config/pulse_server.go
@@ -29,9 +29,11 @@ func MewPulseServerConfig() *PulseServerConfig {
 // It allows users to customize the interval for maestro instance pulses and subscription type.
 //   - "pulse-interval" sets the time between maestro instance pulses (in seconds) to indicate its liveness (default: 15 seconds).
 //   - "subscription-type" specifies the subscription type for resource status updates from message broker, either "shared" or "broadcast".
+//     "shared" subscription type uses MQTT feature to ensure only one Maestro instance receives resource status messages.
+//     "broadcast" subscription type will make all Maestro instances to receive resource status messages and hash the message to determine which instance should process it.
 func (c *PulseServerConfig) AddFlags(fs *pflag.FlagSet) {
-	fs.Int64Var(&c.PulseInterval, "pulse-interval", c.PulseInterval, "Set the pulse interval for maestro instances (seconds) to indicate liveness (default: 10 seconds)")
-	fs.StringVar(&c.SubscriptionType, "subscription-type", c.SubscriptionType, "Set the subscription type for resource status updates from message broker, either \"shared\" or \"broadcast\" (default: \"shared\")")
+	fs.Int64Var(&c.PulseInterval, "pulse-interval", c.PulseInterval, "Sets the pulse interval for maestro instances (seconds) to indicate liveness (default: 10 seconds)")
+	fs.StringVar(&c.SubscriptionType, "subscription-type", c.SubscriptionType, "Sets the subscription type for resource status updates from message broker, Options: \"shared\" (only one instance receives resource status message, MQTT feature ensures exclusivity) or \"broadcast\" (all instances receive messages, hashed to determine processing instance) (default: \"shared\")")
 }
 
 func (c *PulseServerConfig) ReadFiles() error {
