3d774cda557c6337af722ac93ce5ea5d7defbd1f
hchenxa <huichen@redhat.com>
2025-10-11 14:22:00 +0800

    incorrect file permission
    Signed-off-by: hchenxa <huichen@redhat.com>

--- test/e2e/setup/roll_out.sh
+++ test/e2e/setup/roll_out.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-maestro_latest_image=$(curl -s -X GET https://quay.io/api/v1/repository/redhat-user-workloads/maestro-rhtap-tenant/maestro/maestro | jq -s -c 'sort_by(.tags[].last_modified) | .[].tags[] | select(.name | test("sha256-|on-pr-|maestro-on-pull-request")| not) | .name' | head -n 1 | sed 's/\"//g')
-if [ $maestro_latest_image == "" ]; then
+maestro_latest_image_sha=$(curl -s -X GET https://quay.io/api/v1/repository/redhat-user-workloads/maestro-rhtap-tenant/maestro/maestro | jq -s -c 'sort_by(.tags[].last_modified) | .[].tags[] | select(.name | test("sha256-|on-pr-|maestro-on-pull-request")| not) | .name' | head -n 1 | sed 's/\"//g')
+if [ $maestro_latest_image_sha == "" ]; then
     echo "Did not find the maestro latest image, exit!"
     exit 1
 fi
@@ -15,11 +15,10 @@ if [ $(kubectl get deploy -n maestro -l app=maestro --no-headers | wc -l | sed '
         echo "Did not find the maestro image, exit!"
         exit 1
     fi
-    image_repo=$(echo -n $maestro_current_image | awk -F ':' '{print $1}')
     image_sha=$(echo -n $maestro_current_image | awk -F ':' '{print $2}')
-    if [ $image_sha != $maestro_latest_image ]; then
+    if [ $image_sha != $maestro_latest_image_sha ]; then
         # Roll out the maestro
-        kubectl set image deploy/maestro -n maestro *=$image_repo:$maestro_latest_image
+        kubectl set image deploy/maestro -n maestro *=quay.io/redhat-user-workloads/maestro-rhtap-tenant/maestro/maestro@sha256:$maestro_latest_image_sha
         kubectl rollout status deploy/maestro -n maestro --timeout=600s
     fi
 fi
@@ -31,11 +30,10 @@ if [ $(kubectl get deploy -n maestro -l app=maestro-agent --no-headers | wc -l |
         echo "Did not find the maestro agent image, exit!"
         exit 1
     fi
-    image_repo=$(echo -n $maestro_current_image | awk -F ':' '{print $1}')
     image_sha=$(echo -n $maestro_current_image | awk -F ':' '{print $2}')
-    if [ $image_sha != $maestro_latest_image ]; then
+    if [ $image_sha != $maestro_latest_image_sha ]; then
         # Roll out the maestro
-        kubectl set image deploy/maestro-agent -n maestro *=$image_repo:$maestro_latest_image
+        kubectl set image deploy/maestro-agent -n maestro *=quay.io/redhat-user-workloads/maestro-rhtap-tenant/maestro/maestro@sha256:$maestro_latest_image_sha
         kubectl rollout status deploy/maestro-agent -n maestro --timeout=600s
     fi
 fi
