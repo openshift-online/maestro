439894420c3d2c8cc2af610d5c83952a9485aa9f
clyang82 <chuyang@redhat.com>
2025-12-23 16:03:50 +0800

    Add back grpc server tls support
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- test/setup/env_setup.sh
+++ test/setup/env_setup.sh
@@ -22,6 +22,7 @@ istio_version=1.25.5
 
 enable_istio=${ENABLE_ISTIO:-"false"}
 msg_broker=${MESSAGE_DRIVER_TYPE:-"mqtt"}
+tls_enable=${ENABLE_MAESTRO_TLS:-"false"}
 
 export image_tag="latest"
 export external_image_registry="image-registry.testing"
@@ -196,4 +197,33 @@ EOF
   fi
 fi
 
-# Database (PostgreSQL) will be deployed by Helm chart in deploy_server.sh
+if [ "$tls_enable" = "true" ]; then
+  # deploy openshift service-ca to generate certs for internal services (metrics/health)
+  kubectl label node maestro-control-plane node-role.kubernetes.io/master= --overwrite
+  kubectl apply -f "${PWD}/test/setup/service-ca-crds"
+  # kubectl create namespace openshift-config-managed || true
+  kubectl apply -f "${PWD}/test/setup/service-ca"
+  sleep 10 # wait for openshift service-ca-operator is created
+  kubectl wait deploy/service-ca-operator -n openshift-service-ca-operator --for condition=Available=True --timeout=300s
+  sleep 10 # wait for openshift service-ca is created
+  kubectl wait deploy/service-ca -n openshift-service-ca --for condition=Available=True --timeout=300s
+  # prepare gRPC service certs if they are not found
+  grpc_cert_dir="${PWD}/test/_output/certs/grpc"
+  if [ ! -d "$grpc_cert_dir" ]; then
+    # create certs
+    mkdir -p "$grpc_cert_dir"
+    step certificate create "maestro-grpc-ca" ${grpc_cert_dir}/ca.crt ${grpc_cert_dir}/ca.key --kty RSA --profile root-ca --no-password --insecure
+    step certificate create "maestro-grpc-server" ${grpc_cert_dir}/server.crt ${grpc_cert_dir}/server.key --kty RSA -san maestro-grpc -san maestro-grpc.maestro -san localhost -san 127.0.0.1 --profile leaf --ca ${grpc_cert_dir}/ca.crt --ca-key ${grpc_cert_dir}/ca.key --no-password --insecure
+    cat << EOF > ${grpc_cert_dir}/cert.tpl
+{
+    "subject":{"organization":"open-cluster-management","commonName":"grpc-client"},
+    "keyUsage":["digitalSignature"],
+    "extKeyUsage": ["serverAuth","clientAuth"]
+}
+EOF
+    step certificate create "maestro-grpc-client" ${grpc_cert_dir}/client.crt ${grpc_cert_dir}/client.key --kty RSA --template ${grpc_cert_dir}/cert.tpl --ca ${grpc_cert_dir}/ca.crt --ca-key ${grpc_cert_dir}/ca.key --no-password --insecure
+    # create secrets
+    kubectl delete secret maestro-grpc-cert -n "$namespace" --ignore-not-found
+    kubectl create secret generic maestro-grpc-cert -n "$namespace" --from-file=ca.crt=${grpc_cert_dir}/ca.crt --from-file=server.crt=${grpc_cert_dir}/server.crt --from-file=server.key=${grpc_cert_dir}/server.key --from-file=client.crt=${grpc_cert_dir}/client.crt --from-file=client.key=${grpc_cert_dir}/client.key
+  fi
+fi
